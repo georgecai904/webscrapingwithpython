be604c0c55b2986043a3d6f964697e862999ecf1
George Cai <georgemail608@gmail.com>
2017-06-21 12:04:07 +0800

    analyse robots.txt

--- chapter1/crawling.py
+++ chapter1/crawling.py
@@ -14,7 +14,7 @@ def download(url, user_agent='wswp', num_retries=2):
         html = None
         if num_retries > 0:
             if hasattr(e, 'code') and 500 <= e.code < 600:
-                return download(url, user_agent, num_retries-1)
+                return download(url, user_agent, num_retries - 1)
     return html
 
 
@@ -26,10 +26,53 @@ def crawl_sitemap(url):
     # download each link
     for link in links:
         html = download(link)
-        print html
 
 
-seed_url = "http://example.webscraping.com/{0}"
+import urlparse
+import robotparser
+
+
+def link_crawler(seed_url, link_regex, user_agent='wswp', delay=1):
+    """ Crawl from the given seed URL following links matched by link_regex """
+
+    crawl_queue = [seed_url]
+
+    # keep track which URL's have seen before
+    seen = set(crawl_queue)
+
+    # define a robotparser
+    rp = robotparser.RobotFileParser()
+    rp.set_url(seed_url + '/robots.txt')
+    rp.read()
+
+    while crawl_queue:
+        url = crawl_queue.pop()
+        # check url passes robots.txt restrictions
+
+        if rp.can_fetch(user_agent, url):
+            html = download(url)
+            # filter for links matching our regular expression
+            for link in get_links(html):
+                if re.match(link_regex, link):
+                    link = urlparse.urljoin(seed_url, link)
+                    if link not in seen:
+                        seen.add(link)
+                        crawl_queue.append(link)
+        else:
+            print 'Blocked by robots.txt: ', url
+
+
+def get_links(html):
+    """ Return a list of links from html"""
+
+    # a regular expression to extract all links from the webpage
+    webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']', re.IGNORECASE)
+    # list of all links from the webpage
+    return webpage_regex.findall(html)
+
+
+seed_url = "http://example.webscraping.com"
 
 if __name__ == '__main__':
-    crawl_sitemap(seed_url.format("sitemap.xml"))
+    # crawl_sitemap(seed_url+"/places/default/sitemap.xml")
+    link_crawler(seed_url, '/places/default/(index|view)/')
