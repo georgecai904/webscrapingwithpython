2b44894aff9e65f4bc09695192b0580006ad7f29
George Cai <georgemail608@gmail.com>
2017-06-21 12:06:43 +0800

    support proxy

--- chapter1/crawling.py
+++ chapter1/crawling.py
@@ -1,20 +1,28 @@
 import urllib2
 import re
 
-def download(url, user_agent='wswp', num_retries=2):
+
+def download(url, user_agent='wswp', proxy=None, num_retries=2):
     print 'Downloading: ', url
 
     headers = {'User-agent': user_agent}
     request = urllib2.Request(url, headers=headers)
 
+    # Build up proxy
+    opener = urllib2.build_opener()
+    if proxy:
+        proxy_params = {urlparse.urlparse(url).scheme: proxy}
+        opener.add_handler(urllib2.ProxyHandler(proxy_params))
+
     try:
-        html = urllib2.urlopen(request).read()
+        # User proxy opener
+        html = opener.open(request).read()
     except urllib2.URLError as e:
         print 'Download error:', e.reason
         html = None
         if num_retries > 0:
             if hasattr(e, 'code') and 500 <= e.code < 600:
-                return download(url, user_agent, num_retries - 1)
+                return download(url, user_agent, proxy, num_retries - 1)
     return html
 
 
